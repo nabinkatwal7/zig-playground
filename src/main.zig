e54b40d91567a075a3cc1f159b1ccd78b3d05801
Nabin Katwal <nabinkatwal2000@gmail.com>
2026-05-29 12:11:35 +0545

    args

--- src/main.zig
+++ src/main.zig
@@ -1,37 +1,12 @@
 const std = @import("std");
 
-fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+pub fn main(init: std.process.Init.Minimal) void {
+    var args = init.args.iterate();
 
-fn subtract(a: i32, b: i32) i32 {
-    return a - b;
-}
+    var index: usize = 0;
 
-fn multiply(a: i32, b: i32) i32 {
-    return a * b;
-}
-
-const DivisionError = error{
-    DivideByZero,
-};
-
-fn divide(a: i32, b: i32) DivisionError!i32 {
-    if (b == 0) {
-        return DivisionError.DivideByZero;
+    while (args.next()) |arg| {
+        std.debug.print("arg[{d}] = {s}\n", .{ index, arg });
+        index += 1;
     }
-
-    return @divTrunc(a, b);
-}
-
-pub fn main() !void {
-    const add_result = add(10, 20);
-    const subtract_result = subtract(20, 10);
-    const multiply_result = multiply(10, 20);
-    const divide_result = try divide(20, 10);
-
-    std.debug.print("10 + 20 = {}\n", .{add_result});
-    std.debug.print("20 - 10 = {}\n", .{subtract_result});
-    std.debug.print("10 * 20 = {}\n", .{multiply_result});
-    std.debug.print("20 / 10 = {}\n", .{divide_result});
 }
